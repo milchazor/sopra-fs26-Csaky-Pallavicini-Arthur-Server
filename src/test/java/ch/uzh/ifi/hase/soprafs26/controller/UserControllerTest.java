fc47a112f7d7f1d9091837616c2d5a78fbf9f170
milchazor <161317999+milchazor@users.noreply.github.com>
2026-03-05 16:09:23 +0100

    Tests added

--- src/test/java/ch/uzh/ifi/hase/soprafs26/controller/UserControllerTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs26/controller/UserControllerTest.java
@@ -7,6 +7,7 @@ import tools.jackson.databind.ObjectMapper;
 import ch.uzh.ifi.hase.soprafs26.constant.UserStatus;
 import ch.uzh.ifi.hase.soprafs26.entity.User;
 import ch.uzh.ifi.hase.soprafs26.rest.dto.UserPostDTO;
+import ch.uzh.ifi.hase.soprafs26.rest.dto.UserPutDTO;
 import ch.uzh.ifi.hase.soprafs26.service.UserService;
 
 import org.junit.jupiter.api.Test;
@@ -26,8 +27,11 @@ import java.util.List;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -94,6 +98,103 @@ public class UserControllerTest {
 				.andExpect(jsonPath("$.id", is(user.getId().intValue())))
 				.andExpect(jsonPath("$.username", is(user.getUsername())))
 				.andExpect(jsonPath("$.status", is(user.getStatus().toString())));
+	}
+
+	@Test
+	public void createUser_duplicateUsername_returns409() throws Exception {
+		// given
+		UserPostDTO userPostDTO = new UserPostDTO();
+		userPostDTO.setUsername("testUsername");
+		userPostDTO.setPassword("password");
+
+		given(userService.createUser(Mockito.any()))
+				.willThrow(new ResponseStatusException(HttpStatus.CONFLICT, "username already exists"));
+
+		// when
+		MockHttpServletRequestBuilder postRequest = post("/users")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(asJsonString(userPostDTO));
+
+		// then
+		mockMvc.perform(postRequest)
+				.andExpect(status().isConflict());
+	}
+
+	@Test
+	public void getUserById_validId_returnsUser() throws Exception {
+		// given
+		User user = new User();
+		user.setId(1L);
+		user.setUsername("testUsername");
+		user.setToken("1");
+		user.setStatus(UserStatus.ONLINE);
+
+		given(userService.getUserById(1L)).willReturn(user);
+
+		// when
+		MockHttpServletRequestBuilder getRequest = get("/users/1")
+				.contentType(MediaType.APPLICATION_JSON);
+
+		// then
+		mockMvc.perform(getRequest)
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.id", is(user.getId().intValue())))
+				.andExpect(jsonPath("$.username", is(user.getUsername())))
+				.andExpect(jsonPath("$.status", is(user.getStatus().toString())));
+	}
+
+	@Test
+	public void getUserById_invalidId_returns404() throws Exception {
+		// given
+		given(userService.getUserById(99L))
+				.willThrow(new ResponseStatusException(HttpStatus.NOT_FOUND, "user was not found"));
+
+		// when
+		MockHttpServletRequestBuilder getRequest = get("/users/99")
+				.contentType(MediaType.APPLICATION_JSON);
+
+		// then
+		mockMvc.perform(getRequest)
+				.andExpect(status().isNotFound());
+	}
+
+	@Test
+	public void updateUserPassword_validId_returns204() throws Exception {
+		// given
+		UserPutDTO userPutDTO = new UserPutDTO();
+		userPutDTO.setPassword("newPassword");
+
+		doNothing().when(userService).updatePassword(Mockito.eq(1L), Mockito.any(), Mockito.any());
+
+		// when
+		MockHttpServletRequestBuilder putRequest = put("/users/1/password")
+				.contentType(MediaType.APPLICATION_JSON)
+				.header("Authorization", "some-token")
+				.content(asJsonString(userPutDTO));
+
+		// then
+		mockMvc.perform(putRequest)
+				.andExpect(status().isNoContent());
+	}
+
+	@Test
+	public void updateUserPassword_invalidId_returns404() throws Exception {
+		// given
+		UserPutDTO userPutDTO = new UserPutDTO();
+		userPutDTO.setPassword("newPassword");
+
+		doThrow(new ResponseStatusException(HttpStatus.NOT_FOUND, "user was not found"))
+				.when(userService).updatePassword(Mockito.eq(99L), Mockito.any(), Mockito.any());
+
+		// when
+		MockHttpServletRequestBuilder putRequest = put("/users/99/password")
+				.contentType(MediaType.APPLICATION_JSON)
+				.header("Authorization", "some-token")
+				.content(asJsonString(userPutDTO));
+
+		// then
+		mockMvc.perform(putRequest)
+				.andExpect(status().isNotFound());
 	}
 
 	/**
